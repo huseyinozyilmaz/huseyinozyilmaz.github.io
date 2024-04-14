4909831c58deda0dd3d86e84a9fd8ebb34e1ddd2
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2024-04-14 22:00:51 +0100

    Uses ContentList to fetch posts

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,21 +1,26 @@
 <template>
   <div class="py-4 mb-8">
-    <ContentDoc v-slot="{ doc }" 
-      :query="{
+    <ContentDoc :query="{
         isPublished: true
       }">
-      <div class="uppercase p-2 mt-4 bg-gray-900 inline-block text-sm font-medium text-white">Post</div>
-      <article>
-        <h1 class="font-bold text-xl md:text-2xl py-4">{{ doc.title }}</h1>
-        <time :datetime="doc.datetime" class='text-gray-500 text-sm'>Posted on {{ (new Date(doc.datetime)).toDateString() }}</time>
-        <div class='py-6'>
-          <figure class='md:float-left md:pr-5 pb-4'>
-            <img class='rounded-lg w-full md:w-[400px]' :src="doc.image" :alt="doc.title" width="400" height="400" />
-          </figure>
-          <ContentRenderer :value="doc" class="post"/>
-        </div>
-      </article>
-    </ContentDoc>  
+      <template v-slot="{ doc }" > 
+        <div class="uppercase p-2 mt-4 bg-gray-900 inline-block text-sm font-medium text-white">Post</div>
+        <article>
+          <h1 class="font-bold text-xl md:text-2xl py-4">{{ doc.title }}</h1>
+          <time :datetime="doc.datetime" class='text-gray-500 text-sm'>Posted on {{ (new Date(doc.datetime)).toDateString() }}</time>
+          <div class='py-6'>
+            <figure class='md:float-left md:pr-5 pb-4'>
+              <img class='rounded-lg w-full md:w-[400px]' :src="doc.image" :alt="doc.title" width="400" height="400" />
+            </figure>
+            <ContentRenderer :value="doc" class="post"/>
+          </div>
+        </article>
+      </template>
+      <!-- If post is not found -->
+      <template #not-found>
+        <h2 class="text-center py-28">Oppss.. The post is not found!</h2>
+      </template>
+    </ContentDoc>
     <NuxtLink href='/posts' class="text-sm text-center block pt-4 pb-10">Return to all posts</NuxtLink>
   </div>
 </template>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="mb-5">
     <h1 class="text-center text-2xl lg:text-3xl py-8 max-w-[400px] m-auto">{{ app.title }}</h1>
-    <div class='flex gap-6 lg:gap-2 flex-wrap justify-center py-10'>  
-      <div v-for="post in posts" :key="post.uid">
-        <PostItem :post="post" />
-      </div>
+    <div class='flex gap-6 lg:gap-2 flex-wrap justify-center py-10'>
+      <ContentList path="/posts" v-slot="{ list }" 
+        :query="{
+          where: { isPublished: true }
+        }">
+        <div v-for="post in list" :key="post._path">
+          <PostItem :post="post" />
+        </div>
+      </ContentList>
     </div>
   </div>
 </template>
 
 <script setup>
 const app = useAppConfig()
-
-const { data: posts } = await useAsyncData('posts', () => queryContent('posts')
-  .only(['_path', 'uid', 'title', 'headline', 'date', 'image'])
-  .where({ isPublished: true })
-  .find())
-
 </script>
