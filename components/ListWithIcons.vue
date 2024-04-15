9b828a673e9c239af523838952d4a97257606dfb
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2024-04-15 14:07:24 +0100

    Adds missing key for v-for

--- components/ListWithIcons.vue
+++ components/ListWithIcons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex gap-4 flex-wrap justify-center">
-    <div v-for="item in items" class="flex gap-2 items-center w-40 h-28 justify-end flex-col" >
+    <div v-for="item in items" :key="item.icon" class="flex gap-2 items-center w-40 h-28 justify-end flex-col" >
       <div class="">
         <Icon :name="item.icon" size="32" />  
       </div>
