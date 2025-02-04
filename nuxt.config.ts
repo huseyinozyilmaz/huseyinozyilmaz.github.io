53d533fbdcc83dfb46d135b2f873790d688cfd2f
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2025-02-04 11:46:07 +0000

    Adds compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,26 +2,32 @@
 export default defineNuxtConfig({
   ssr: true,
   devtools: { enabled: false },
+  compatibilityDate: '2025-02-04',
   telemetry: false,
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   // Removes client side hydration by removing JSON Payloads
   experimental: {
     payloadExtraction: false,
     renderJsonPayloads: false
   },
+
   // Removes client side hydration by removing all JavaScripts
   routeRules: {
     '/': { prerender: true, experimentalNoScripts: true },
     '/posts/**': { prerender: true, experimentalNoScripts: true }
   },
+
   nitro: {
     preset: 'static'
   },
+
   modules: ["@nuxt/content"]
 })
